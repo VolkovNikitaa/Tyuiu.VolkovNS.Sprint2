d924170b52bebbe7b310c2f19769e80804a22caf
Никита Волков <Tyuiu.VolkovNS@outlook.com>
2025-09-29 08:38:16 +0300

    Task0

---
+++ Tyuiu.VolkovNS.Sprint2.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.VolkovNS.Sprint2.Task0.V10.Lib;
+
+namespace Tyuiu.VolkovNS.Sprint2.Task0.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 1305;
+            int y = 475;
+            bool[] res = new bool[6] { false, true, false, true, false, true };
+            CollectionAssert.AreEqual(res,ds.GetCompareOperations(x,y));
+        }
+    }
+}
---
+++ Tyuiu.VolkovNS.Sprint2.Task0.V10/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.VolkovNS.Sprint2.Task0.V10.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds= new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил Волков Н. С. | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Операции сравнения                                                *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнил: Волков Никита Сергеевич | ПКТб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+        Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+        Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+        Console.WriteLine("*  (False, True, False, True, False, True), при x = 1305, y = 475         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ: X=1305 Y=475                                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int x = 1305, y = 475;
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write(ds.GetCompareOperations(x, y)[i] + " ");
+        }
+    }
+}
